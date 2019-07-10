5ab2b62d648a680b4dbad839731746eb811d1a19
tragoni <52310656+tragoni@users.noreply.github.com>
2019-07-10 11:02:43 +0900

    use jQuery

--- colors.js
+++ colors.js
@@ -1,20 +1,23 @@
 var Body = {
   setBackgroundColor : function(color) {
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
   },
   setColor : function(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color',color);
   }
 }
 
 var Links = {
    setColor : function(color) {
-     var alist = document.querySelectorAll('a');
-     var i = 0;
-     while(i<alist.length) {
-       alist[i].style.color=color;
-       i++;
-     }
+     // var alist = document.querySelectorAll('a');
+     // var i = 0;
+     // while(i<alist.length) {
+     //   alist[i].style.color=color;
+     //   i++;
+     // }
+     $('a').css('color',color);
    }
 }
 
